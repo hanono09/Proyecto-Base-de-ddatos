cb1ae9ea51f74d405ab1ccfe250c1e50d5c6b2fa
hanono09 <fedehanono@gmail.com>
2025-05-22 10:36:41 -0300

    ej 1 y 2 terrminado y ej 3 empezado.

---
+++ index.js
@@ -0,0 +1,83 @@
+console.log("Hola 4C")
+
+let objeto1 = 
+{
+    nombre: "Juan",
+    apellido: "Gomez",
+    dni: 46345678,
+    anio: 4,
+    curso: "A",
+    nota: 6,
+}
+let objeto2 = 
+{
+    nombre: "Lamine",
+    apellido: "Yamal   ",
+    dni: 46345678,
+    anio: 4,
+    curso: "A",
+    nota: 10,
+}
+let objeto3 = 
+{
+    nombre: "Simone",
+    apellido: "Hernandez",
+    dni: 48995678,
+    anio: 5,
+    curso: "A",
+    nota: 8,
+}
+let objeto4 = 
+{
+    nombre: "Hernan",
+    apellido: "traverso",
+    dni: 48175678,
+    anio: 5,
+    curso: "A",
+    nota: 5,
+}
+let objeto5 = 
+{
+    nombre: "Hernan",
+    apellido: "Bale",
+    dni: 46345678,
+    anio: 4,
+    curso: "b",
+    nota: 3,
+}
+let objeto6 = 
+{
+    nombre: "Camila",
+    apellido: "Delciopo",
+    dni: 46345678,
+    anio: 4,
+    curso: "b",
+    nota: 1,
+}
+let objeto7 = 
+{
+    nombre: "Camila",
+    apellido: "Delciopo",
+    dni: 46345678,
+    anio: 3,
+    curso: "b",
+    nota: 4,
+}
+let objeto8 = 
+{
+    nombre: "Jose",
+    apellido: "Da Vinci",
+    dni: 46345678,
+    anio: 3,
+    curso: "b",
+    nota: 9,
+}
+let array = [objeto1, objeto2, objeto3, objeto4, objeto5, objeto6, objeto7, objeto8]
+
+function capitalizar(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+console.log(capitalizar("legoland"))
+
+function MostrarAlumno(objeto1)
+console.log(MostrarAlumno)
